8a81cc0f0b3de7d2ab5afe3b8b88154ca081118f
Tenshi-X <sevafarel17@gmail.com>
2023-03-19 10:27:28 +0700

    Add Tugas1 Login and Logout System

---
+++ Tugas_1/tugas1/src/main/java/com/pbo/tugas1/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pbo.tugas1;
+
+/**
+ *
+ * @author Lenovo
+ */
+import com.pbo.tugas1.Pages.loginPage;
+
+
+public class Main {
+    public static void main(String[] args) {
+       new loginPage().setVisible(true);
+    }
+}
